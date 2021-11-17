cdd59a4721657e7950e6a1b81893f698b91e1aa6
mayur0977 <mayur0977@users.noreply.github.com>
2021-11-17 14:57:11 +0530

    feat : initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as ClassicEditorBuild from '@ckeditor/ckeditor5-build-classic';
+import { ChangeEvent } from '@ckeditor/ckeditor5-angular/ckeditor.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ckeditor';
+  public editor = ClassicEditorBuild;
+  data: any = `<b>bold text</><ul><li>op 1</li><li>op 2</li></ul>`;
+  public isDisabled = false;
+  toggleDisabled() {
+    this.isDisabled = !this.isDisabled;
+  }
+  public onReady(editor: any) {
+    console.log('editor', editor.config);
+
+    editor.ui
+      .getEditableElement()
+      .parentElement.insertBefore(
+        editor.ui.view.toolbar.element,
+        editor.ui.getEditableElement()
+      );
+  }
+  public onChange({ editor }: ChangeEvent) {
+    const data = editor.getData();
+
+    console.log(data);
+  }
+
+  ngOnInit(): void {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    CKEditorModule,
     AppRoutingModule
   ],
   providers: [],
